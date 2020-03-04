6ab19bd40ba71b6ff7767c12de60e5d2dd0689f6
Henry Calderon <theceintiant@gmail.com>
2020-03-04 00:37:59 -0800

    Starting new branch for the JSON Data

---
+++ Divvy/Marketplace/JSONTest/JSONgathering.swift
@@ -0,0 +1,10 @@
+//
+//  JSONgathering.swift
+//  Divvy
+//
+//  Created by Henry Calderon on 3/4/20.
+//  Copyright © 2020 Jessica Trinh. All rights reserved.
+//
+
+import Foundation
+// to import stuff here?
