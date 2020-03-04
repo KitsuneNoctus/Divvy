4ef1ae7a589aef63a6bc0e23076cd05f6483088b
Henry Calderon <theceintiant@gmail.com>
2020-03-04 00:49:38 -0800

    Started working but a lot ahead.

--- Divvy/Marketplace/JSONTest/JSONgathering.swift
+++ Divvy/Marketplace/JSONTest/JSONgathering.swift
@@ -8,3 +8,25 @@
 
 import Foundation
 // to import stuff here?
+
+//let data = "oldOutput.json"
+//let json = try? JSON(data: jsonData)
+
+let fileName = "testoldOutput.json"
+
+func readJSONFromFile(fileName: String) -> Any?
+{
+    var json: Any?
+    if let path = Bundle.main.path(forResource: fileName, ofType: "json") {
+        do {
+            let fileUrl = URL(fileURLWithPath: path)
+            // Getting data from JSON file using the file URL
+            let data = try Data(contentsOf: fileUrl, options: .mappedIfSafe)
+            json = try? JSONSerialization.jsonObject(with: data)
+        } catch {
+            // Handle error here
+            print("There was an error")
+        }
+    }
+    return json
+}
